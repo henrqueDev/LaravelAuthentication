d3b06cb69e3e16c7907dcb9417f41cfd1e7c3571
henrqueDev <lhdeveloper137@gmail.com>
2024-04-09 08:50:02 -0300

    feat - site guards

--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,10 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+        'site' => [
+            'driver' => 'session',
+            'provider' => 'users',
+        ],
     ],
 
     /*
